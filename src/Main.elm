fe35029b9a70c970a5ebb7761c306690e33b01f0
Dwayne Crooks <me@dwaynecrooks.com>
2020-10-30 15:50:58 -0400

    Complete "Orientation"

--- src/Main.elm
+++ src/Main.elm
@@ -1,8 +1,13 @@
 module Main exposing (main)
 
 
-import Html exposing (text)
+import Html exposing (div, h2, p, text)
+import Html.Attributes exposing (class)
 
 
 main =
-    text "Hello, world"
+    div [ class "jumbo" ]
+        [ div [ class "right tomster" ] []
+        , h2 [] [ text "Welcome to Super Rentals!" ]
+        , p [] [ text "We hope you find exactly what you're looking for in a place to stay." ]
+        ]
